f839e0a8a5594dd39c4ad60a353ecfc98bac95fe
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-04-02 12:01:18 -0500

    Creation of views and permissions

--- back-end/todolist/views.py
+++ back-end/todolist/views.py
@@ -3,6 +3,14 @@ from todolist.models import Task
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework.response import Response
+from todolist.permissions import isOwnerOrReadOnly
+from todolist.serializers import TaskSerializer, UserSerializer
 
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, isOwnerOrReadOnly]

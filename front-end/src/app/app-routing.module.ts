b59aaeed1dfe14b2d51ee53e467238acdc62507f
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-04-03 09:26:30 -0500

    Login design

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LoginComponent},
+  {path:'tasks', component: TasksComponent},
+  {path:'**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
